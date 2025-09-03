8756859c8535932192b868d679c823632b7dc6ff
muhammad-tamim <contact2tamim@gmail.com>
2025-09-04 02:38:48 +0600

    feat: display vidoe category buttons

--- scripts/script.js
+++ scripts/script.js
@@ -8,3 +8,18 @@ function loadVideoCategoryButtons() {
 
 loadVideoCategoryButtons()
 
+function displayVideoCategoryButtons(categoriesData) {
+
+    const categoryButtonsContainer = document.getElementById("category-buttons-container");
+
+    for (let data of categoriesData) {
+        const div = document.createElement("div");
+        div.innerHTML = `
+        <button id="${data.category_id}"
+            class="font-medium text-primary-content/70 py-2 px-5 rounded-[4px] bg-[#252525]/15 cursor-pointer hover:bg-primary
+            hover:text-white hover:font-semibold">${data.category}</button>
+        `
+        categoryButtonsContainer.appendChild(div);
+
+    }
+}
