9f0520d5e8694d8d4f70e265c5098e15bd23f93c
muhammad-tamim <contact2tamim@gmail.com>
2025-09-04 03:36:49 +0600

    feat: show all category video based on the category video buttons

--- scripts/script.js
+++ scripts/script.js
@@ -14,7 +14,7 @@ function displayVideoCategoryButtons(categoriesData) {
     for (let data of categoriesData) {
         const div = document.createElement("div");
         div.innerHTML = `
-        <button id="${data.category_id}"
+        <button onclick="displayCategoryVideoById(${data.category_id})" id="${data.category_id}"
             class="font-medium text-primary-content/70 py-2 px-5 rounded-[4px] bg-[#252525]/15 cursor-pointer hover:bg-primary
             hover:text-white hover:font-semibold">${data.category}</button>
         `
@@ -35,47 +35,21 @@ loadAllVideoData();
 
 function displayAllVideoDataThroughCards(allVideoData) {
 
+    showOrHideErrorMessage("show")
+
     document.getElementById("display-all-video-btn").addEventListener("click", () => {
-        const videoCardsContainer = document.getElementById("video-cards-container")
-
-        for (let data of allVideoData) {
-
-            const showFormatDate = formatDate(data.others.posted_date)
-
-            const div = document.createElement("div");
-            div.innerHTML = `
-            <div class="card">
-                <figure class="relative">
-                    <img class="rounded-lg w-[312px] h-[200px]"
-                        src="${data.thumbnail}" alt="Shoes" />
-                    <span
-                        class="absolute text-xs right-10 bottom-3 rounded-md bg-primary-content py-1 px-[5px] text-white">
-                        ${showFormatDate}
-                    </span>
-                </figure>
-                <div class="card-body">
-                    <div class="flex gap-3">
-                        <div class="avatar">
-                            <div class="rounded-full size-10">
-                                <img src="${data.authors[0].profile_picture}" />
-                            </div>
-                        </div>
-                        <div class="space-y-2">
-                            <h3 class="font-bold text-base text-primary-content">${data.title}</h3>
-                            <p class="flex items-center gap-2 text-sm text-primary-content/70">
-                            ${data.authors[0].profile_name}
-                            ${data.authors[0].verified
-                    ? `<img class="size-5" src="assets/images/icons8-verified-48.png"
-                            alt="">`
-                    : ""} 
-                            </p> 
-                            <p class="text-sm text-primary-content/70"><span>${data.others.views}</span> Views</p>
-                        </div >
-                    </div >
-                </div >
-            </div >
-                `
-            videoCardsContainer.appendChild(div);
-        }
+        showVideoOnCards(allVideoData)
+        showOrHideErrorMessage("hide")
     })
+}
+
+
+function displayCategoryVideoById(id) {
+    const url = `https://openapi.programming-hero.com/api/phero-tube/category/${id}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            showVideoOnCards(data.category)
+        })
+
 }
--- utilities/utilities.js
+++ utilities/utilities.js
@@ -19,3 +19,64 @@ function formatDate(date) {
     // assume the data have already in milliseconds format
     return new Date(num).toDateString()
 }
+
+
+function showOrHideErrorMessage(value) {
+    if (value === "show") {
+        document.getElementById("error-message").classList.remove("hidden")
+    }
+    else if (value === "hide") {
+        document.getElementById("error-message").classList.add("hidden")
+    }
+}
+
+function showVideoOnCards(videoData) {
+
+    const videoCardsContainer = document.getElementById("video-cards-container")
+    videoCardsContainer.innerHTML = ""
+
+    if (videoData.length === 0) {
+        showOrHideErrorMessage("show")
+    }
+
+
+    for (let data of videoData) {
+
+        const showFormatDate = formatDate(data.others.posted_date)
+
+        const div = document.createElement("div");
+        div.innerHTML = `
+            <div class="card">
+                <figure class="relative">
+                    <img class="rounded-lg w-[312px] h-[200px]"
+                        src="${data.thumbnail}" alt="Shoes" />
+                    <span
+                        class="absolute text-xs right-10 bottom-3 rounded-md bg-primary-content py-1 px-[5px] text-white">
+                        ${showFormatDate}
+                    </span>
+                </figure>
+                <div class="card-body">
+                    <div class="flex gap-3">
+                        <div class="avatar">
+                            <div class="rounded-full size-10">
+                                <img src="${data.authors[0].profile_picture}" />
+                            </div>
+                        </div>
+                        <div class="space-y-2">
+                            <h3 class="font-bold text-base text-primary-content">${data.title}</h3>
+                            <p class="flex items-center gap-2 text-sm text-primary-content/70">
+                            ${data.authors[0].profile_name}
+                            ${data.authors[0].verified
+                ? `<img class="size-5" src="assets/images/icons8-verified-48.png"
+                            alt="">`
+                : ""} 
+                            </p> 
+                            <p class="text-sm text-primary-content/70"><span>${data.others.views}</span> Views</p>
+                        </div >
+                    </div >
+                </div >
+            </div >
+                `
+        videoCardsContainer.appendChild(div);
+    }
+}
